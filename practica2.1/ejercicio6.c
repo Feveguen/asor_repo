247a628416e45a85255bb4409129ad5f489fd256
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 13:23:06 +0100

    Update ejercicio6.c

--- practica2.1/ejercicio6.c
+++ practica2.1/ejercicio6.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
+int main(){
+int name;
+if(sysconf(name) ==-1){
+printf("ERROR: (%d). %s \n", errno, strerror(errno));
+return -1;
+}
+
+else{
+
+printf("Longitud maxima de argumentos: %d\n",sysconf(_SC_ARG_MAX));
+printf("Numero maximo de ficheros abiertos por proceso: %d\n",sysconf(_SC_OPEN_MAX));
+printf("Numero maximo de hijos: %d\n",sysconf(_SC_CHILD_MAX));
+}
+
+return 1;
+}
+
+//Longitud maxima de argumentos: 2097152
+//Numero maximo ficheros abiertos por proceso: 1024
+//Numero maximo hijos: 3836
