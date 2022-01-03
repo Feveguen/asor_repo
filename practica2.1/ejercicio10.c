8b4a79cc34d6a63f0dd57f8f12b11fb7d6eb55cd
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 17:51:21 +0100

    Update ejercicio10.c

--- practica2.1/ejercicio10.c
+++ practica2.1/ejercicio10.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
+int main(){
+
+printf("El UID real es: %d\n", getuid());
+printf("El UID efectivo es: %d\n", geteuid());
+
+struct passwd *pw;
+
+printf("Nombre de usuario: %d\n",pw->pw_name);
+printf("Directorio home: %d\n",pw->pw_dir);
+printf("Descripcion del usuario: %d\n",pw->pw_gecos);
+
+return 1;
+
+}
