aa1baf6755fe9b7383398f842cbdb3d25ed4b872
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 13:31:49 +0100

    Update ejercicio7.c

--- practica2.1/ejercicio7.c
+++ practica2.1/ejercicio7.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
+int main(){
+int name;
+if(pathconf("/",name) ==-1){
+printf("ERROR: (%d). %s \n", errno, strerror(errno));
+return -1;
+}
+
+else{
+
+printf("Numero maximo de enlaces: %d\n",pathconf("/",_PC_LINK_MAX));
+printf("Longitud maxima del nombre de fichero: %d\n",pathconf("/",_PC_NAME_MAX));
+printf("Longitud maxima de una ruta relativa: %d\n",pathconf("/",_PC_PATH_MAX));
+}
+
+return 1;
+}
+
+//Numero maximo de enlaces: 2147483647
+//Longitud maxima del nombre de fichero: 255
+//Longitud maxima de una ruta relativa: 4096
