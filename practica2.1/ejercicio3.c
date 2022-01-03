be16478639679fd7362cb3191759db986a138635
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 12:39:11 +0100

    Update ejercicio3.c

--- practica2.1/ejercicio3.c
+++ practica2.1/ejercicio3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 
+const int MAX_ERRORS=255;
+
+int main (){
+char *s;
+int i;
+for(i=0; i < MAX_ERRORS;i++){
+printf("ERROR(%d): %s. \n", i,strerror(i));
+  
+  //Va a haber 133 errores segun la compilación
+}
+
+return 1;
+}
