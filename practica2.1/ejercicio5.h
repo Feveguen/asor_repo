fc86b2cfed19aeaf0d5e6a710eb114a6b71d6bfd
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 13:10:15 +0100

    Update ejercicio5.h

--- practica2.1/ejercicio5.h
+++ practica2.1/ejercicio5.h
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/utsname.h>
 
+int main(){
+struct utsname buff;
+
+if(uname(&buff) ==-1){
+printf("ERROR: (%d). %s \n", errno, strerror(errno));
+return -1;
+}
+
+else{
+
+printf("Nombre del sistema operativo: %d\n",buff.sysname);
+printf("Nombre del host: %d\n", buff.nodename);
+printf("Release del host: %d\n", buff.release);
+printf("Version del SO: %d\n", buff.version);
+printf("Hardware: %d\n", buff.machine);
+}
+
+return 1;
+}
