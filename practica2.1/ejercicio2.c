8e09f403a09a45bc9d336d5080b7e7b051b2fbbd
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 11:39:28 +0100

    Update ejercicio2.c

--- practica2.1/ejercicio2.c
+++ practica2.1/ejercicio2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 
+int main (){
+
+char *s;
+int res= setuid(0);
+if(res== -1){
+printf("ERROR(%d): %s.", errno,strerror(errno));
+}
+
+return 1;
+}
