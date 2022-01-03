ee6fd03056679e228df85e05b8eac2613f03d615
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 17:56:36 +0100

    Create ejercicio11.c

---
+++ practica2.1/ejercicio11.c
@@ -0,0 +1,17 @@
+DATE(1)                      User Commands                      DATE(1)
+
+NAME
+       date - print or set the system date and time
+
+SYNOPSIS
+       date [OPTION]... [+FORMAT]
+       date [-u|--utc|--universal] [MMDDhhmm[[CC]YY][.ss]]
+
+DESCRIPTION
+       Display  the current time in the given FORMAT, or set the system
+       date.
+
+       Mandatory arguments to long  options  are  mandatory  for  short
+       options too.
+
+//Diferentes opciones para date.
