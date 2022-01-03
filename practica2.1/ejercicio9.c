0f1dbd45b08ccb74798b6f503f343dfea8ca8d67
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-03 15:52:56 +0100

    Update ejercicio9.c

--- practica2.1/ejercicio9.c
+++ practica2.1/ejercicio9.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
 
+int main(){
+
+printf("El UID real es: %d\n", getuid());
+printf("El UID efectivo es: %d\n", geteuid());
+
+return 1;
+
+}
+
+
+//Cuando el EUID del proceso se cambia al usuario del fichero
