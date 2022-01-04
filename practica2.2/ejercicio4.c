5861b96aaa32058bec4ba330167cd85708ae319d
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 12:12:04 +0100

    Update ejercicio4.c

--- practica2.2/ejercicio4.c
+++ practica2.2/ejercicio4.c
@@ -1,1 +1,5 @@
-
+[cursoredes@localhost ~]$ cd Downloads/p2
+[cursoredes@localhost p2]$ mkdir ejer4
+[cursoredes@localhost p2]$ chmod 666 ejer4
+[cursoredes@localhost p2]$ cd ejer4
+bash: cd: ejer4: Permission denied
