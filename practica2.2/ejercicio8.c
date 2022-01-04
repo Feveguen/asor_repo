14689930960d57aed17b6a7d0d226da91721eb12
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 12:36:44 +0100

    Create ejercicio8.c

---
+++ practica2.2/ejercicio8.c
@@ -0,0 +1,8 @@
+[cursoredes@localhost p2]$ man stat
+[cursoredes@localhost p2]$ ls -i
+34138721 1             34138716 ejercicio2.c  33654088 ejercicio7
+  460960 dir           34138719 ejercicio3.c  34138724 ejercicio7.c
+50520302 ej4           33974368 ejercicio5    33807464 fic
+17301011 ejer4         33974372 ejercicio5c
+34138715 ejercicio1.c  34138722 ejercicio5.c
+[cursoredes@localhost p2]$ 
