d9110399dfcb4b154741bb5f09da7dd279470a1c
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 12:35:30 +0100

    Update ejercicio7.c

--- practica2.2/ejercicio7.c
+++ practica2.2/ejercicio7.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+int main(){
+
+mode_t mask=umask(750);
+int o=open("/home/cursoredes/Downloads/p2/ejercicio7.c", 0645);
+  
+return 0;
+}
