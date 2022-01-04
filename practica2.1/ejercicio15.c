e8b2b99a8a745d1b0a60d526353c403f67d38438
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 10:23:28 +0100

    Update ejercicio15.c

--- practica2.1/ejercicio15.c
+++ practica2.1/ejercicio15.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
+#include <locale.h>
 
+int main(){
+
+time_t ti=time(NULL);
+struct tm *t=localtime(&ti);
+setlocale(LC_ALL, "");
+
+char fecha[1000];
+
+strftime(fecha, 1000, "%A ," " %d de " " %B de " "%Y ," "%H:%M", t);
+printf("Hoy es: %f\n", fecha);
+
+return 0;
+
+}
