baf05b0cc79551ad33e2793f64e447dd96d9e3db
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 12:27:07 +0100

    Update ejercicio5.c

--- practica2.2/ejercicio5.c
+++ practica2.2/ejercicio5.c
@@ -1,3 +1,16 @@
-[root@localhost p2]# gcc ejercicio5.c -o ejercicio5c
-[root@localhost p2]# ls -l ejercicio5.c
--rw-r--r-x 1 root root 184 Jan  4 12:25 ejercicio5.c
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(){
+
+int o=open("/home/cursoredes/Downloads/p2/ejercicio5.c", 0645, O_CREAT);
+
+return 0;
+}
+
+//[root@localhost p2]# gcc ejercicio5.c -o ejercicio5c
+//[root@localhost p2]# ls -l ejercicio5.c
+//rw-r--r-x 1 root root 184 Jan  4 12:25 ejercicio5.c
