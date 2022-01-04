cee93fa896a064eedc5df5e154d3bd057ca7993f
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 12:01:05 +0100

    Update ejercicio2.c

--- practica2.2/ejercicio2.c
+++ practica2.2/ejercicio2.c
@@ -1,1 +1,4 @@
-
+[cursoredes@localhost ~]$ ls -ld Desktop
+drwxr-xr-x 2 cursoredes cursoredes 116 Sep  9  2018 Desktop
+[cursoredes@localhost ~]$ ls -ld Downloads/
+drwxr-xr-x 4 cursoredes cursoredes 26 Jan  4 10:41 Downloads/
