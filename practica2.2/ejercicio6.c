7002e9bc7e287dde8625a7e2556bdc5af720ebcf
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 12:31:19 +0100

    Update ejercicio6.c

--- practica2.2/ejercicio6.c
+++ practica2.2/ejercicio6.c
@@ -1,1 +1,7 @@
-
+[cursoredes@localhost Practica2]$ umask 750
+[cursoredes@localhost Practica2]$ mkdir ej6
+[cursoredes@localhost Practica2]$ cd ej6
+bash: cd: ej6: Permission denied
+[cursoredes@localhost Practica2]$ touch ej6
+[cursoredes@localhost Practica2]$ ls -l ej6
+ls: cannot open directory ej6: Permission denied
