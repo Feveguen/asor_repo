3fb63815b59763c970a9440d727b9c6b150a3224
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-04 09:47:48 +0100

    Update ejercicio13.c

--- practica2.1/ejercicio13.c
+++ practica2.1/ejercicio13.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
 
+int main(){
+
+struct timeval tv;
+
+int ini = gettimeofday(&tv,NULL);
+int start = tv.tv_usec;
+int i;
+  
+for(i =0; i < 1000000; i++){
+}
+
+int fin=gettimeofday(&tv,NULL);
+int end = tv.tv_usec;
+
+printf("El tiempo que tarda es: %d\n", end-start);
+
+return 0;
+}
+
+
+//El tiempo que tarda es: 4594
