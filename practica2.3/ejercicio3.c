5531c9f8f90902c8b1d7bdc1e95764560c05664b
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-05 12:38:15 +0100

    Update ejercicio3.c

--- practica2.3/ejercicio3.c
+++ practica2.3/ejercicio3.c
@@ -1,1 +1,9 @@
-
+[root@localhost p3]# sudo chrt --fifo --pid 12 19765
+[root@localhost p3]# gcc ejercicio2.c -o ejercicio2
+[root@localhost p3]# ./ejercicio2
+El pid es: 20067
+El pid padre es: 19765
+La planificacion es: FIFO
+La prioridad es: 0
+La prioridad maxima es: 99
+La prioridad minima es: 1
