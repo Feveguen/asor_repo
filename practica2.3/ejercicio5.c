ccc97f9f772717466aa46b09c93a1c1d1e130bcf
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-05 13:53:47 +0100

    Update ejercicio5.c

--- practica2.3/ejercicio5.c
+++ practica2.3/ejercicio5.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main(){
 
+pid_t pidh=getpid();
+pid_t pidp=getppid();
+pid_t pidg=getpgid(pidh);
+pid_t pids=getsid(pidh);
+
+char buf[60];
+char *c=getcwd(buf, 60);
+
+printf("El PID es: %d\n", pidh);
+printf("El PPID es: %d\n", pidp);
+printf("El GPID es: %d\n", pidg);
+printf("El SID es: %d\n", pids);
+printf("La ruta es: %d\n", c);
+
+return 0;
+}
