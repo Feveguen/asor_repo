6356c3d5704270ba05b2913d115a20cc4b2d84a7
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-06 12:46:55 +0100

    Update ejercicio7a.c

--- practica2.3/ejercicio7a.c
+++ practica2.3/ejercicio7a.c
@@ -1,1 +1,32 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+
+if(argc!=2){
+
+printf("Error: Mal uso de los parametros en el ejercicio: %d\n", argv[0]);
+return -1;
+
+}
+
+pid_t pid=fork();
+
+if(pid==0){
+
+int res= system(argv[1]);
+
+if(res==-1){
+perror("Fallo en el system");
+return -1;
+}
+
+printf("El comando terminó de ejercutarse.\n");
+
+}
+
+return 0;
+}
