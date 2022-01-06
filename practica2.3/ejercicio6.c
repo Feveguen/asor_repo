4d30b89f3affe62d8b2fa79627d22f75dde597b6
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-06 12:30:56 +0100

    Update ejercicio6.c

--- practica2.3/ejercicio6.c
+++ practica2.3/ejercicio6.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
+int main(){
+
+pid_t p_hijo=fork();
+
+if(p_hijo==0){ //Hijo
+
+setsid();
+int dir=chdir("/tmp");
+
+printf("El PID es: %d\n", getpid());
+printf("El PPID es: %d\n", getppid());
+printf("El GPID es: %d\n", getpgrp());
+printf("El SID es: %d\n", getsid(getpid()));
+printf("La ruta es: %d\n", dir);
+
+}
+
+pause();
+
+return 0;
+}
