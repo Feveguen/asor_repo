e3150c01017aa13535b3b1ec565c75ac284efe83
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-07 11:35:03 +0100

    Update ejercicio11.c

--- practica2.3/ejercicio11.c
+++ practica2.3/ejercicio11.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
 
+int main(){
+
+//Declaramos variable
+sigset_t blk_sig;
+
+//Creamos conjunto y añadimos señales
+sigemptyset(&blk_sig);
+sigaddset(&blk_sig,SIGINT);
+sigaddset(&blk_sig,SIGTSTP);
+
+//Bloqueamos las señales del conjunto
+sigprocmask(SIG_BLOCK, &blk_sig, NULL);
+
+//A dormir x seg
+sleep(6);
+
+//Comprobamos si se recibió
+sigset_t pending;
+if(sigismember(&pending,SIGINT)==1){
+printf("Se recibió SIGINT\n");
+  
+//Se borra
+sigdelset(&blk_sig,SIGINT);
+}
+
+
+
+//Comprobamos si se recibió
+if(sigismember(&pending,SIGTSTP)==1){
+printf("Se recibió SIGTSTP\n");
+
+//Se borra
+sigdelset(&blk_sig,SIGTSTP);
+}
+
+//Desbloqueamos las señales del conjunto
+sigprocmask(SIG_UNBLOCK, &blk_sig, NULL);
+  
+return 0;
+
+}
