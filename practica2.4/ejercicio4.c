3fae023f2968715f47157bcb74f5001e0dcc46c3
Miguel <36701370+Feveguen@users.noreply.github.com>
2022-01-10 17:38:59 +0100

    Update ejercicio4.c

--- practica2.4/ejercicio4.c
+++ practica2.4/ejercicio4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+int main(int argc, char *argv[]){
+
+
+if(argc !=2){
+printf("Error en el uso de comandos");
+return -1;
+}
+
+
+int fd= open("/tuberiaconnombre", O_WRONLY);
+write(fd,argv[1],strlen(argv[1]));
+close(fd);
+
+return 0;
+}
